2c4d54aebeefaf1941532541cb842d2c0e1db8bb
Enos <enoshenrique008@gmail.com>
2024-11-17 22:52:59 -0300

    start project

---
+++ src/main/java/Enos/SpringProject/literAlura/LiterAluraApplication.java
@@ -0,0 +1,20 @@
+package Enos.SpringProject.literAlura;
+
+import Enos.SpringProject.literAlura.main.Main;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.start();
+	}
+}
---
+++ src/main/java/Enos/SpringProject/literAlura/main/Main.java
@@ -0,0 +1,8 @@
+package Enos.SpringProject.literAlura.main;
+
+public class Main {
+
+    public void start(){
+        System.out.println("Hello World");
+    }
+}
